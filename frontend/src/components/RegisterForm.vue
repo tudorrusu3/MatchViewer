b26a75dbbbfc25af6cfdd4685dd885fc1dd12d0a
tudorrusu3 <tudorrusu133@yahoo.com>
2025-07-15 21:33:23 +0300

    resolved bug in register page

--- frontend/src/components/RegisterForm.vue
+++ frontend/src/components/RegisterForm.vue
@@ -1,31 +1,28 @@
 <template>
-    <div class="register-container">
-  <v-card
-    class=" pa-6"
-    max-width="448"
-    style="min-width: 300px; width: 100%;" 
-  >
-  <v-card-title class="text-h3 text-center font-weight-bold">Create account</v-card-title>
+  <div class="register-container">
+    <v-card class="pa-6" max-width="448" style="min-width: 300px; width: 100%;">
+      <v-card-title class="text-h3 text-center font-weight-bold">Create account</v-card-title>
+
       <v-text-field
         v-model="name"
         label="Name"
         density="compact"
-        variant="outlined" 
+        variant="outlined"
         rounded="lg"
-        bg-color="#eeeeee" 
+        bg-color="#eeeeee"
         color="#3c4ebe"
         prepend-inner-icon="mdi-account"
       ></v-text-field>
 
       <v-text-field
+        v-model="email"
         label="Email"
-        v-model="email" 
-        density="compact" 
-        prepend-inner-icon="mdi-email-outline" 
-        variant="outlined" 
-        rounded="lg" 
-        bg-color="#eeeeee" 
+        density="compact"
+        variant="outlined"
+        rounded="lg"
+        bg-color="#eeeeee"
         color="#3c4ebe"
+        prepend-inner-icon="mdi-email-outline"
       ></v-text-field>
 
       <v-text-field
@@ -33,95 +30,134 @@
         label="Password"
         density="compact"
         variant="outlined"
-        prepend-inner-icon="mdi-lock-outline" 
-        rounded="lg" 
+        rounded="lg"
         bg-color="#eeeeee"
         color="#3c4ebe"
+        prepend-inner-icon="mdi-lock-outline"
+        type="password"
       ></v-text-field>
 
       <v-text-field
         v-model="confirmPassword"
         label="Re-enter password"
-        variant="outlined"
         density="compact"
-        prepend-inner-icon="mdi-lock-outline" 
-        rounded="lg" 
+        variant="outlined"
+        rounded="lg"
         bg-color="#eeeeee"
         color="#3c4ebe"
-        
+        prepend-inner-icon="mdi-lock-outline"
+        type="password"
       ></v-text-field>
 
-    <v-btn class="mb-5 mt-2" color="#3c4ebe" size="large" block @click="register" rounded="lg">
-          Create your account
-        </v-btn>
-        <v-card-text class="text-center">
-          <a @click="goTologin" class=" text-decoration-none" href="#" style="color: #3c4ebe">
-            Already have an account? Sign in
-            <v-icon icon="mdi-chevron-right"></v-icon>
-          </a>
-        </v-card-text>
+      <v-btn
+        class="mb-5 mt-2"
+        color="#3c4ebe"
+        size="large"
+        block
+        @click="register"
+        rounded="lg"
+      >
+        Create your account
+      </v-btn>
 
-  </v-card>
-</div>
+      <router-link to="/login" class="text-decoration-none" style="color: #3c4ebe;">
+          Already have an account? Sign in
+  <v-icon icon="mdi-chevron-right"></v-icon>
+</router-link>
+
+
+    </v-card>
+  </div>
+
+  <v-snackbar
+    v-model="snackbar.visible"
+    :color="snackbar.color"
+    :location="snackbar.location"
+    :elevation="snackbar.elevation"
+    timeout="3000"
+  >
+    {{ snackbar.message }}
+  </v-snackbar>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       name: '',
       email: '',
-      password: ''
+      password: '',
+      confirmPassword: '',
+      snackbar: {
+        visible: false,
+        message: '',
+        color: 'red',
+        elevation: '2',
+        location: 'top',
+      },
     };
   },
   methods: {
     async register() {
+      if (this.password !== this.confirmPassword) {
+        this.showSnackbar('Parolele nu coincid', 'red');
+        return;
+      }
 
       try {
         const response = await fetch('http://localhost:3000/register', {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json' 
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            name: this.name,
+            userName: this.name,
             email: this.email,
-            password: this.password
-          })
+            password: this.password,
+          }),
         });
+
         const data = await response.json();
         console.log(data);
-        
+
         if (response.ok) {
-          this.$router.push('/login');
+          this.showSnackbar('Cont creat cu succes', 'green');
+          setTimeout(() => {
+            this.$router.push('/');
+          }, 2000);
+        } else if (response.status === 409) {
+          this.showSnackbar('Emailul este deja folosit', 'red');
         } else {
-          alert('Registration failed: ' + data.message || 'Something went wrong');
+          this.showSnackbar(data?.error || 'A apărut o eroare la înregistrare', 'red');
         }
       } catch (error) {
         console.error(error);
-        alert('Registration failed');
+        this.showSnackbar('Eroare de rețea sau server', 'red');
       }
     },
-    
+
+    showSnackbar(message, color = 'red') {
+      this.snackbar.message = message;
+      this.snackbar.color = color;
+      this.snackbar.visible = true;
+    },
+
     goTologin() {
       this.$router.push('/login');
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style >
+<style>
 .register-container {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  
 }
+
 .v-btn {
   font-size: 16px;
 }
-
-
 </style>
